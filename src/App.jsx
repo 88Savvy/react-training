ad0d939b59a7c12260d33946aa40eae48c14d4f2
88Savvy <129120206+88Savvy@users.noreply.github.com>
2023-06-06 20:37:22 -0300

    iterations 1 - 13 complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Fetch from './components/Fetch'
+
+function App() {
+
+
+  return (
+    <div>
+    <h1>Use Effect & Data Fetching</h1>
+    <Fetch />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Fetch.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import { useState } from "react";
+
+export default function Fectch() {
+  
+  const [products, setProducts] = useState([])
+    return (
+    <div>
+      <h2>Fetch</h2>
+    </div>
+  );
+}
